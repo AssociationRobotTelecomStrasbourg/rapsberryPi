fce624fd02fa1a0d6e140bb3104388ee5a853a79
sorgann <44338555+Sorgann@users.noreply.github.com>
2018-12-15 15:49:34 +0000

    Modfify detection parameters

--- atomDetection/detectionTest2.py
+++ atomDetection/detectionTest2.py
@@ -3,30 +3,38 @@ import numpy as np
 from matplotlib import pyplot as plt
 import timeit
 
-file = "../imageRasp/atom/distTestBlackFloor/image4.jpg"
+file = "/home/pi/raspberryPi/imageRasp/atom/terrainAtom/image6.jpg"
 image = cv2.imread(file)
 
 imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV);
+# plt.figure()
+# plt.imshow(imgHSV[:,:,2])
 
-threshV = (imgHSV[:,:,2]>40).astype('uint8')*255
-
+threshV = (imgHSV[:,:,2]>200).astype('uint8')*255
+plt.figure()
+plt.imshow(threshV)
+# plt.figure()
+# plt.hist(imgHSV[:,:,2].ravel(),255)
 params = cv2.SimpleBlobDetector_Params()
 
-params.filterByCircularity = False
-params.filterByColor = True
+params.filterByCircularity = True
+params.filterByColor = False
 params.filterByConvexity = True
 params.filterByInertia = False
 
-params.minThreshold = 0
+params.minThreshold = 48
 params.maxThreshold = 50
 params.thresholdStep = 1
-params.filterByArea = True
 
+params.filterByArea = True
 params.minArea = 500
 params.maxArea = 30000
 
 params.minConvexity = 0.9
 params.maxConvexity = 1
+
+params.minCircularity = 0.5
+params.maxCircularity = 1
 
 params.blobColor = 255
 
@@ -37,13 +45,17 @@ start = timeit.timeit()
 
 detectorBlob = cv2.SimpleBlobDetector_create(params)
 
-keypoints = detectorBlob.detect(imgHSV[:,:,2])
+keypoints = detectorBlob.detect(threshV)
+# keypoints = detectorBlob.detect(imgHSV[:,:,2])
+
+print("Nb keypoints " + str(len(keypoints)))
 
 for key in keypoints:
     cv2.circle(image, (int(key.pt[0]),int(key.pt[1])) , 5, (255,0,0), -1)
 
-print(start -  timeit.timeit())
+print(timeit.timeit()-start)
 
+plt.figure()
 plt.imshow(image)
 plt.show()
 
